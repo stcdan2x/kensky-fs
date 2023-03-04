32ddbab5f3ab04e6d660b9f83e5285a79cd90770
Steven Dandan <stcdan2x@gmail.com>
2023-03-04 15:04:47 +0800

    adds progress bar cdn to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Kenzky Auctions</title>
-        @routes
-        @vite('resources/js/app.js')
-        @inertiaHead
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body class="bg-gray-300 dark:bg-gray-800 text-black dark:text-white antialiased">
-        @inertia
-    </body>
+    <title>Kenzky Auctions</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+
+    @routes
+    @vite('resources/js/app.js')
+    @inertiaHead
+
+</head>
+
+<body class="bg-gray-300 dark:bg-gray-800 text-black dark:text-white antialiased">
+    @inertia
+</body>
+
 </html>
