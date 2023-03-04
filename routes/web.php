dcbfbfcf55e78edb02348932d629007b9b661843
Steven Dandan <stcdan2x@gmail.com>
2023-03-04 15:06:50 +0800

    adds UserAccountController routes and modifies ListingController routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,15 +12,20 @@ use App\Http\Controllers\ListingController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/test', [IndexController::class, 'test']);
+Route::get('/hello', [IndexController::class, 'show'])
+   ->middleware('auth');
 
-Route::resource('listing', ListingController::class);
+Route::resource('listing', ListingController::class)
+   ->only(['create', 'store', 'edit', 'update', 'destroy'])
+   ->middleware('auth');
+Route::resource('listing', ListingController::class)
+   ->except(['create', 'store', 'edit', 'update', 'destroy']);
 
 Route::get('login', [AuthController::class, 'create'])
    ->name('login');
@@ -27,3 +33,6 @@ Route::post('login', [AuthController::class, 'store'])
    ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
    ->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+   ->only(['create', 'store']);
